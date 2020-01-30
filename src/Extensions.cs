b8d91e5a8710bbdec1fa6bed761432c0c5c7f56a
Chris Sainty <chris@chrissainty.com>
2020-01-30 14:36:55 +0000

    Initial commit

---
+++ src/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorFragmentNavigation
+{
+    public static class Extensions
+    {
+        public static ValueTask NavigateToFragmentAsync(this NavigationManager navigationManager, IJSRuntime jSRuntime)
+        {
+            var uri = navigationManager.ToAbsoluteUri(navigationManager.Uri);
+
+            if (uri.Fragment.Length == 0)
+            {
+                return default;
+            }
+            return jSRuntime.InvokeVoidAsync("blazorHelpers.scrollToFragment", uri.Fragment.Substring(1));
+        }
+    }
+}
---
+++ src/Pages/FragmentNavigationBase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorFragmentNavigation.Pages
+{
+    public class FragmentNavigationBase : ComponentBase, IDisposable
+    {
+        [Inject] NavigationManager NavManager { get; set; }
+        [Inject] IJSRuntime JsRuntime { get; set; }
+
+        protected override void OnInitialized()
+        {
+            NavManager.LocationChanged += TryFragmentNavigation;
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await NavManager.NavigateToFragmentAsync(JsRuntime);
+            }
+        }
+
+        private async void TryFragmentNavigation(object sender, LocationChangedEventArgs args)
+        {
+            await NavManager.NavigateToFragmentAsync(JsRuntime);
+        }
+
+        public void Dispose()
+        {
+            NavManager.LocationChanged -= TryFragmentNavigation;
+        }
+    }
+}
